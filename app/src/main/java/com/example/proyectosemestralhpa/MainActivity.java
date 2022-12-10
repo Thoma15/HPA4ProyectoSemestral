50b4b676cfaf12645670166e556e34dc6476a34b
Maria Flores <mariaflores790@gmail.com>
2022-12-10 01:33:53 -0500

    Sensor 3: detector de pasos Se creo su Activity y el Java. Y probando a ver si funciona el git

--- app/src/main/java/com/example/proyectosemestralhpa/MainActivity.java
+++ app/src/main/java/com/example/proyectosemestralhpa/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener {
-    String[] opciones = {"Presentación", "Sensor 1 Proximidad", "Sensor 2 Reconocimiento","Sensor 3 Detector", "Sensor 4 Orientación"};
-    String[] msg = {"Presentación", "Sensor 1 Proximidad", "Sensor 2 Reconocimiento","Sensor 3 Detector", "Sensor 4 Orientación"};
+    String[] opciones = {"Presentación", "Sensor 1 Proximidad", "Sensor 2 Reconocimiento", "Sensor 3 Detector", "Sensor 4 Orientación"};
+    String[] msg = {"Presentación", "Sensor 1 Proximidad", "Sensor 2 Reconocimiento", "Sensor 3 Detector", "Sensor 4 Orientación"};
     private Button button;
     private int pos;
 
@@ -25,45 +25,44 @@ public class MainActivity extends AppCompatActivity implements
         Spinner spin = (Spinner) findViewById(R.id.spinner);
         spin.setOnItemSelectedListener(this);
         ArrayAdapter aa = new
-                ArrayAdapter(this,android.R.layout.simple_spinner_item,opciones);
+                ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones);
 
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         ;
         spin.setAdapter(aa);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 changeActivity(pos);
             }
         });
     }
+
     private void changeActivity(int pos) {
-        if(pos == 0){
+        if (pos == 0) {
             Intent intent = new Intent(this, Presentacion.class);
             startActivity(intent);
-        }
-        else if(pos == 1){
+        } else if (pos == 1) {
             Intent intent = new Intent(this, Sensor1.class);
             startActivity(intent);
-        }
-        else if(pos == 2){
+        } else if (pos == 2) {
             Intent intent = new Intent(this, Sensor2.class);
             startActivity(intent);
-        }
-        else if(pos == 3){
+        } else if (pos == 3) {
             Intent intent = new Intent(this, Sensor3.class);
             startActivity(intent);
-        }
-        else if(pos == 4){
+        } else if (pos == 4) {
             Intent intent = new Intent(this, Sensor4.class);
             startActivity(intent);
         }
+    }
         @Override
-        public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        public void onItemSelected (AdapterView < ? > arg0, View arg1,int position, long id){
             pos = position;
-            Toast.makeText(getApplicationContext(), msg[position] , Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), msg[position], Toast.LENGTH_LONG).show();
         }
+
         @Override
-        public void onNothingSelected(AdapterView<?> adapterView) {
+        public void onNothingSelected (AdapterView < ? > adapterView){
         }
     }
--- app/src/main/java/com/example/proyectosemestralhpa/Sensor3.java
+++ app/src/main/java/com/example/proyectosemestralhpa/Sensor3.java
@@ -1,14 +1,161 @@
 package com.example.proyectosemestralhpa;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class Sensor3 extends AppCompatActivity {
+public class Sensor3 extends Activity implements SensorEventListener{
+
+
+    // Gravity for accelerometer data
+    private float[] gravity = new float[3];
+    // smoothed values
+    private float[] smoothed = new float[3];
+    // sensor manager
+    private SensorManager sensorManager;
+    // sensor gravity
+    private Sensor sensorGravity;
+    private double bearing = 0;
+
+    private TextView acc;
+    private TextView stepView;
+    private TextView thresholdView;
+    private SeekBar seek;
+    private ToggleButton countToggle;
+
+    private int stepCount;
+    private boolean toggle;
+    private double prevY;
+    private double threshold;
+    private boolean ignore;
+    private int countdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor3);
+        acc = (TextView) findViewById((R.id.accelerometer));
+        thresholdView = (TextView) findViewById((R.id.thresholdView));
+        stepView = (TextView) findViewById((R.id.stepView));
+        countToggle = (ToggleButton) findViewById(R.id.countToggle);
+        seek = (SeekBar) findViewById(R.id.seek);
+
+        seek.setProgress(0);
+        seek.incrementProgressBy(1);
+        seek.setMax(40);
+        // keep screen light on (wake lock light)
+
+        implementListeners();
+    }
+
+    protected float[] lowPassFilter( float[] input, float[] output ) {
+        if ( output == null ) return input;
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + 1.0f * (input[i] - output[i]);
+        }
+        return output;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorGravity = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // listen to these sensors
+        sensorManager.registerListener(this, sensorGravity,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // remove listeners
+        sensorManager.unregisterListener(this, sensorGravity);
+    }
+
+
+
+
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // get accelerometer data
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            // we need to use a low pass filter to make data smoothed
+            smoothed = lowPassFilter(event.values, gravity);
+            gravity[0] = smoothed[0];
+            gravity[1] = smoothed[1];
+            gravity[2] = smoothed[2];
+            //acc.setText("x: "+gravity[0] + " y: " + gravity[1] + " z: " + gravity[2]+ "ignore: "+ ignore + "countdown: "+ countdown);
+            if(ignore) {
+                countdown--;
+                ignore = (countdown < 0)? false : ignore;
+            }
+            else
+                countdown = 22;
+            if(toggle && (Math.abs(prevY - gravity[1]) > threshold) && !ignore){
+                stepCount++;
+                stepView.setText("" + stepCount);
+                ignore = true;
+            }
+            prevY = gravity[1];
+        }
+    }
+
+    public void implementListeners(){
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                threshold = ((double)seek.getProgress()) * 0.02;
+                thresholdView.setText(""+ threshold);
+            }
+        });
+
+        countToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle = !toggle ;
+                if(toggle){
+                    stepCount = 0;
+                    countdown = 5;
+                    ignore = true;
+                    stepView.setText("" + stepCount);
+                }
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
