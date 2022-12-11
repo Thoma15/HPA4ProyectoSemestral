d19f2e6adc27de5d98afa8a0964e94492f9ca5c1
Thoma15 <104921999+Thoma15@users.noreply.github.com>
2022-12-10 19:47:20 -0500

    Update MainActivity.java
    Actualizado sin los pos en las condiciones

--- app/src/main/java/com/example/proyectosemestralhpa/MainActivity.java
+++ app/src/main/java/com/example/proyectosemestralhpa/MainActivity.java
@@ -4,65 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements
-        AdapterView.OnItemSelectedListener {
-    String[] opciones = {"Presentación", "Sensor 1 Proximidad", "Sensor 2 Reconocimiento", "Sensor 3 Detector", "Sensor 4 Orientación"};
-    String[] msg = {"Presentación", "Sensor 1 Proximidad", "Sensor 2 Reconocimiento", "Sensor 3 Detector", "Sensor 4 Orientación"};
-    private Button button;
-    private int pos;
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spin;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-        Spinner spin = (Spinner) findViewById(R.id.spinner);
-        spin.setOnItemSelectedListener(this);
-        ArrayAdapter aa = new
-                ArrayAdapter(this, android.R.layout.simple_spinner_item, opciones);
-
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        ;
+        spin = (Spinner) findViewById(R.id.spin);
+        String[] opciones = {"Sensor 1 Proximidad", "Sensor 2 Gestos","Sensor 3 Detector", "Sensor 4 Orientación"};
+        ArrayAdapter<String> aa = new
+                ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,opciones);
         spin.setAdapter(aa);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                changeActivity(pos);
-            }
-        });
-    }
-
-    private void changeActivity(int pos) {
-        if (pos == 0) {
-            Intent intent = new Intent(this, Presentacion.class);
-            startActivity(intent);
-        } else if (pos == 1) {
-            Intent intent = new Intent(this, Sensor1.class);
-            startActivity(intent);
-        } else if (pos == 2) {
-            Intent intent = new Intent(this, Sensor2.class);
-            startActivity(intent);
-        } else if (pos == 3) {
-            Intent intent = new Intent(this, Sensor3.class);
-            startActivity(intent);
-        } else if (pos == 4) {
-            Intent intent = new Intent(this, Sensor4.class);
-            startActivity(intent);
-        }
-    }
-        @Override
-        public void onItemSelected (AdapterView < ? > arg0, View arg1,int position, long id){
-            pos = position;
-            Toast.makeText(getApplicationContext(), msg[position], Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        public void onNothingSelected (AdapterView < ? > adapterView){
-        }
-    }
+            public void onClick(View view){
+                //int pos = 0;
+               // changeActivity(pos);
+                //private void changeActivity(int pos) {
+                String selec = spin.getSelectedItem().toString();
+                     if(selec.equals("Sensor 1 Proximidad")){
+                        Intent intent = new Intent(MainActivity.this, Sensor1.class);
+                        startActivity(intent);
+                    }
+                    else if(selec.equals("Sensor 2 Reconocimiento")){
+                        Intent intent = new Intent(MainActivity.this, Sensor2.class);
+                        startActivity(intent);
+                    }
+                    else if(selec.equals("Sensor 3 Detector")){
+                        Intent intent = new Intent(MainActivity.this, Sensor3.class);
+                        startActivity(intent);
+                    }
+                    else if(selec.equals("Sensor 4 Orientación")){
+                        Intent intent = new Intent(MainActivity.this, Sensor4.class);
+                        startActivity(intent);
+                    }}
+            });}}
